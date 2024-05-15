c3ae18d37128e71c87ced9a4dcb190d65385854b
chaseunggeun <122505891+chaseunggeun@users.noreply.github.com>
2024-05-15 16:45:42 +0900

    [Silver II] Title: 에디터, Time: 148 ms, Memory: 20880 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int main()
+{
+    int M;
+    string s = "";
+    string ans = "";
+
+    cin >> s;
+    list<char> li(s.begin(), s.end());
+
+    auto cursor = li.end();
+    cin >> M;
+
+    for (int i = 0; i < M; i++)
+    {
+        char cmd, c;
+        cin >> cmd;
+
+        if (cmd == 'L')
+        {
+            if (cursor != li.begin())
+                cursor--;
+        }
+        else if (cmd == 'D')
+        {
+            if (cursor != li.end())
+                cursor++;
+        }
+        else if (cmd == 'B')
+        {
+            if (cursor != li.begin())
+            {
+                cursor--;
+                cursor = li.erase(cursor);
+            }
+        }
+        else if (cmd == 'P')
+        {
+            cin >> c;
+            li.insert(cursor, c);
+        }
+    }
+
+    for (cursor = li.begin(); cursor != li.end(); cursor++)
+        cout << *cursor;
+}
