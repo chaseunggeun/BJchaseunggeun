aceb1468446d985a4d067e390dd2507b54b1d5a3
chaseunggeun <122505891+chaseunggeun@users.noreply.github.com>
2024-09-23 14:14:12 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+long long dp[1001][1001];
+int n, k;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> k;
+
+	dp[0][0] = 1;
+	dp[1][0] = 1;
+	dp[1][1] = 1;
+
+	for (int i = 2; i <= n; i++)
+		for (int j = 0; j <= i; j++)
+		{
+			if (j == 0) dp[i][0] = 1;
+			else if (j == i) dp[i][j] = 1;
+			else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+		}
+
+
+	cout << dp[n][k];
+}
